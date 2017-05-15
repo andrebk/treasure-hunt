4fc9d0d95509192f93add3605a31c56c2ed060a1
Andreas Bro Kolstø <andreas.kolsto@gmail.com>
2017-05-15 19:03:22 +1000

    Make agent update map as player moves around
    Agent will store observed tiles in its map. Also adds printMap function
    but it will only print tile type, not items.

--- Agent.java
+++ Agent.java
@@ -10,54 +10,204 @@ import java.io.*;
 import java.net.*;
 
 public class Agent {
-	
-	private Tile[][] map = new Tile[164][164];
-	private int startingPoint = 80;
-	
-	
-	private void fillTable(char view[][], int[] position){
-		int posx = position[0];
-		int posy = position[1];
-		for(int i = 0; i<5; i++){
-			for(int j=0; j<5; j++){
-			    int tileX = posx-2+j;
-			    int tileY = posy-2+i;
-			    Tile newTile = new Tile(view[j][i], view[j][i], tileX, tileY);
-				this.map[tileX][tileY] = newTile;
-			}
-		}
-	}
-	
-	
+
+    private Tile[][] map = new Tile[164][164];
+    private int start = 82;
+    private int x = start;
+    private int y = start;
+
+    private final static int EAST = 0;
+    private final static int NORTH = 1;
+    private final static int WEST = 2;
+    private final static int SOUTH = 3;
+
+    private int direction = NORTH;
+
+
+    private void updateMap(char view[][]) {
+        int row, col;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == 2 && j == 2) {
+                    // Players position. Skip so player icon is not stored in map.
+                    continue;
+                }
+
+                // Transform coordinates to rotate view position to correct map position
+                switch (direction) {
+                    case NORTH:
+                        row = i;
+                        col = j;
+                        break;
+                    case WEST:
+                        row = 5 - 1 - j;
+                        col = i;
+                        break;
+                    case EAST:
+                        row = j;
+                        col = 5 - 1 - i;
+                        break;
+                    case SOUTH:
+                        row = 5 - 1 - i;
+                        col = 5 - 1 - j;
+                        break;
+                    default:
+                        throw new RuntimeException("Invalid direction");
+                }
+
+                int tileY = y - 2 + row;
+                int tileX = x - 2 + col;
+
+                if (map[tileY][tileX] == null) {
+                    Tile newTile = new Tile(view[i][j], view[i][j], tileX, tileY);
+                    map[tileY][tileX] = newTile;
+                }
+                else {
+                    map[tileY][tileX].setType(view[i][j]);
+                    map[tileY][tileX].setItem(view[i][j]);
+                }
+            }
+        }
+    }
+
+    private void printMap(){
+        char ch = ' ';
+        char[] line = new char[164];
+        System.out.println();
+        boolean printLine;
+
+        for (int y = 0; y < 164; y++) {
+            printLine = false;
+
+            for (int x = 0; x < 164; x++) {
+
+                if (map[y][x] == null) {
+                    ch = '?';
+                }
+                else if(x == this.x && y == this.y) {
+                    // If at player position, print player icon in correct orientation
+                    switch (direction) {
+                        case NORTH:
+                            ch = '^';
+                            break;
+                        case EAST:
+                            ch = '>';
+                            break;
+                        case WEST:
+                            ch = '<';
+                            break;
+                        case SOUTH:
+                            ch = 'v';
+                            break;
+                    }
+                    printLine = true;
+                }
+                else {
+                    ch = map[y][x].getType();
+                    printLine = true;
+                }
+
+                line[x] = ch;
+            }
+            if (printLine) {
+                // Print this line of the map, if it has explored parts
+                System.out.format("y: %3d  ", y);
+                System.out.println(line);
+            }
+        }
+        System.out.println();
+    }
+
+
+    private char getHumanAction() throws IOException{
+        int ch = 0;
+        System.out.print("Enter Action(s): ");
+
+        while (ch != -1) {
+            // read character from keyboard
+            ch = System.in.read();
+            ch = Character.toLowerCase(ch);
+
+            // Return the action if it is valid
+            switch (ch) {
+                case 'f':
+                case 'l':
+                case 'r':
+                case 'c':
+                case 'u':
+                case 'b':
+                    return ((char) ch);
+            }
+        }
+        throw new RuntimeException("Error occurred when getting player input");
+    }
+
+    private void updateState(char ch) {
+        int deltaX = 0;
+        int deltaY = 0;
+        int nextX, nextY;
+        Tile nextTile;
+
+        switch (direction) {
+            case EAST:
+                deltaX = 1;
+                break;
+            case WEST:
+                deltaX = -1;
+                break;
+            case NORTH:
+                deltaY = -1;
+                break;
+            case SOUTH:
+                deltaY = 1;
+                break;
+        }
+
+        // Find the tile in front of the player
+        nextX = x + deltaX;
+        nextY = y + deltaY;
+        nextTile = map[nextY][nextX];
+
+        // Update agents world state
+        switch (ch) {
+            case 'f':
+                switch (nextTile.getType()) {
+                    case '*':
+                    case '-':
+                    case 'T':
+                        // Do not update position
+                        break;
+                    default:
+                        x = nextX;
+                        y = nextY;
+                        break;
+                }
+                break;
+            case 'l':
+                direction = (direction + 1) % 4;
+                break;
+            case 'r':
+                direction = (direction + 3) % 4;
+                break;
+            case 'c':
+            case 'u':
+            case 'b':
+        }
+    }
+
     public char get_action(char view[][]) {
 
         // REPLACE THIS CODE WITH AI TO CHOOSE ACTION
 
-        int ch = 0;
-
-        System.out.print("Enter Action(s): ");
+        updateMap(view);
+        printMap();
 
         try {
-            while (ch != -1) {
-                // read character from keyboard
-                ch = System.in.read();
+            char ch = getHumanAction();
 
-                switch (ch) { // if character is a valid action, return it
-                    case 'F':
-                    case 'L':
-                    case 'R':
-                    case 'C':
-                    case 'U':
-                    case 'B':
-                    case 'f':
-                    case 'l':
-                    case 'r':
-                    case 'c':
-                    case 'u':
-                    case 'b':
-                        return ((char) ch);
-                }
-            }
+            updateState(ch);
+
+            return ch;
         } catch (IOException e) {
             System.out.println("IO error:" + e);
         }
