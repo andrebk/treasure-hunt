e51cf6753f3d90aee99feb1a4cffbf1286ed5188
Henrik Willett <henrikwillett@192-168-1-4.tpgi.com.au>
2017-05-18 13:50:09 +1000

    logic class

---
+++ Logic.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Logic {
+	
+	public void exploreMap(Agent agent){
+		Tile start = agent.getTile(82, 82);
+		Queue<Tile> queue = new LinkedList<Tile>();
+		start.setSeen();
+		queue.add(start);
+		while(queue != null){
+			Tile n = queue.poll();
+			int x = n.getX();
+			int y = n.getY();
+			// Check WEST tile
+			if(agent.getTile(x-1, y).getSeen() == false){
+				agent.getTile(x-1, y).setSeen();
+				queue.add(agent.getTile(x-1, y));
+			}
+			// Check EAST tile
+			else if(agent.getTile(x+1, y).getSeen() == false){
+				agent.getTile(x+1, y).setSeen();
+				queue.add(agent.getTile(x+1, y));
+			}
+			// Check NORTH tile
+			else if(agent.getTile(x, y+1).getSeen() == false){
+				agent.getTile(x, y+1).setSeen();
+				queue.add(agent.getTile(x, y+1));
+			}
+			// Check SOUTH tile
+			else if(agent.getTile(x, y-1).getSeen() == false){
+				agent.getTile(x, y-1).setSeen();
+				queue.add(agent.getTile(x, y-1));
+			}
+		}
+		return;
+	}
+
+}
