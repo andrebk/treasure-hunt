a8fb39f72d39ec651ba87964d319463ad3a49cb2
Andreas Bro Kolstø <andreas.kolsto@gmail.com>
2017-05-20 17:29:40 +1000

    Flesh out SearchState class a bit
    This is a bit of a intermediary commit

--- SearchState.java
+++ SearchState.java
@@ -3,6 +3,7 @@ import java.util.PriorityQueue;
 public class SearchState {
     private Tile[][] map;
     private int posX, posY;
+    private int targetX, targetY;
 
     private int dynamites = 0;
     private boolean hasDynamite = false;
@@ -14,10 +15,9 @@ public class SearchState {
     private int direction = Agent.NORTH;
 
     private char[] prevActions;
+    //TODO: Store previous states as well? Check for repetion
     private int cost = 0;
     private int heuristic = 0;
-
-    PriorityQueue actions;
 
     //TODO: List of known treasure positions?
     //TODO: List of known item positions?
@@ -25,19 +25,18 @@ public class SearchState {
 
     SearchState(Tile[][] map, int x, int y, int dynamites, boolean hasAxe, boolean hasKey, boolean hasRaft, boolean hasTreasure, int dir) {
         this.map = map; //TODO: Is this a new map, or a reference to the old one?
+        //TODO: Maybe do a deep copy of the map, and then pass the reference to later states?
+        //TODO: Might not work, as it could be hard to undo changes when going back in the search tree.
         this.posX = x;
         this.posY = y;
         this.dynamites = dynamites;
-        if (dynamites > 0) {
-            hasDynamite = true;
-        } else {
-            hasDynamite = false;
-        }
+        this.hasDynamite = dynamites > 0;
         this.hasAxe = hasAxe;
         this.hasKey = hasKey;
         this.hasRaft = hasRaft;
         this.hasTreasure = hasTreasure;
         this.direction = dir;
+        //TODO: Set target coordinates
         //TODO: Set cost, heuristic and prevActions
     }
 
@@ -52,15 +51,18 @@ public class SearchState {
         this.hasRaft = state.hasRaft;
         this.hasTreasure = state.hasTreasure;
         this.direction = state.direction;
+        this.targetX = state.targetX;
+        this.targetY = state.targetY;
         //TODO: Set cost, heuristic and prevActions
     }
 
     SearchState(SearchState state, char action) {
         this(state);
         //TODO: Add action to prevActions and update costs
+        //TODO: Update state, as in agent code
     }
 
-    public void expandState(){
+    public void expandState() {
         //TODO: Expand this state. Create new states for all possible actions, and calculate their cost/heuristic
     }
 
@@ -72,18 +74,50 @@ public class SearchState {
         return cost;
     }
 
-    public void setCost(int cost) {
-        //TODO: Set based on previous actions
-        this.cost = cost;
+    public void setCost(char action) {
+        //TODO: Improve costs. Extra cost for moving to land and losing the raft. Bomb cost dependent on number of bombs?
+        switch (action) {
+            case 'r':
+            case 'l':
+            case 'f':
+                this.cost++;
+                break;
+            case 'c':
+            case 'u':
+                this.cost++;
+                break;
+            case 'b':
+                this.cost += 20;
+                break;
+        }
     }
 
     public int getHeuristic() {
         return heuristic;
     }
 
-    public void setHeuristic(int h) {
-        //TODO: Set based on the actual goal
-        this.heuristic = h;
+    public void setHeuristic() {
+        // Manhattan distance
+        this.heuristic = Math.abs(targetX - posX) + Math.abs(targetY - posY);
     }
 
+    public int getFCost() {
+        return getCost() + getHeuristic();
+    }
+
+    private boolean canMoveForward() {
+
+    }
+
+    private boolean canCutTree() {
+
+    }
+
+    private boolean canUnlock() {
+
+    }
+
+    private boolean canBlowUp() {
+
+    }
 }
